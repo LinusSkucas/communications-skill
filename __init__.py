c3204798d5a3c95cb1fb82a562685b1bbb8638d9
Linus S <LinusS1@users.noreply.github.com>
2019-06-19 20:39:39 -0700

    Get rid of old server and client code

--- __init__.py
+++ __init__.py
@@ -1,67 +1,25 @@
 from mycroft import MycroftSkill, intent_file_handler
-import requests
-import json
+# TODO: import new client code
 
 class Communications(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     def initialize(self):
-        self.add_event('skill.communications.intercom.new', self.handle_new_intercom)
+        self.add_event('skill.communications.intercom.new', self.handle_new_intercom)  # TODO: Get recieving code to messagebus message here
         # Start the server
 
-
-    def get_ips(self, message_type):
-        """Get the ip addresses of Mycroft devices
-        Returns a list of ip addresses with ports
+    def send_intercom(self, message):
+        """Send messages to all other devices
         """
-        #Get the ips
-        # todo: tell user to set up device
-        if message_type == "intercom":
-            mycroft_ips = self.settings.get("mycroft_ips")
-            self.log.info("Intercom")
-            self.settings["mycroft_ips"] = ["10.0.1.36"]
-
-        # TODO: do a messaging - Only find one ip
-
-        ips = []
-
-        # add ports
-        for ip in mycroft_ips:
-            ips.append(self._add_port(ip))
-
-        return ips
-
-
-    def _add_port(self, ip):
-        """Adds the port to the ip address"""
-        # todo: Should make this port configurable
-        return ip +":8090"
-
-    def _make_request(self, server, url, message):
-        """make the request to the other web servers"""
-        requests.post("http://"+server+url, json={"message": message})
-
-    def make_request(self, servers, url, message):
-        """Send multiple requests to multiple servers"""
-        for server in servers:
-            self._make_request(server, url, message)
-
-    def send_message(self, message, message_type):
-        """Send messages to other devices
-        Intercom -> To all devices
-        Messaging -> To a specific device
-        """
-        if message_type == "intercom":
-            mycroft_ips = self.get_ips(message_type)
-            self.make_request(mycroft_ips, "/communications/intercom/new", message)
-        elif message_type == "message":
-            pass
+        # TODO: Connect to new sending code
+        pass
 
     def handle_new_intercom(self, message):
         """A intercom was called"""
         # Get the announcement
         announcement = message.data.get("message")
+        # TODO: Make a bling sound!
         self.speak_dialog("new.intercom", data={"message": announcement})
 
     @intent_file_handler('broadcast.intercom.intent')
@@ -69,12 +27,11 @@ class Communications(MycroftSkill):
         # Get the announcement
         announcement = message.data.get("announcement")
         while not announcement:
-            announcement = self.get_response("make.new.announcement")
+            announcement = self.get_response("get.new.announcement.name.dialog")
 
         # OKay, we got the announcement
         # Time to send the message to all...
         self.send_message(announcement, "intercom")
-
         self.speak_dialog('broadcasting.intercom')
 
 
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Deploying..."
-sleep 1
-echo "Getting Ready..."
-skill_dir=$(pwd)
-cd ..
-rsync -avz --del $skill_dir pi@10.0.1.7:/opt/mycroft/skills/
-echo "Done!"
