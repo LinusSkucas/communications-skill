fc74fab091ec468eddaefc0759185e95797b6882
Linus S <LinusS1@users.noreply.github.com>
2019-06-23 16:20:10 -0700

    Allow broadcasting intercoms using the new server/client
    Signed-off-by: Linus S <LinusS1@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from mycroft import MycroftSkill, intent_file_handler
-from . import shippingHandling
-import py2p
 import threading
 
+import py2p
+from mycroft import MycroftSkill, intent_file_handler
+
+from . import shippingHandling
 
 
 class Communications(MycroftSkill):
@@ -23,15 +24,15 @@ class Communications(MycroftSkill):
     def send_intercom(self, message):
         """Send messages to all other devices
         """
-        # TODO: Connect to new sending code
-        pass
+        shippingHandling.send_message(self.sock, message, message_type="intercom")
 
     def handle_new_intercom(self, message):
         """A intercom was called"""
         # Get the announcement
         announcement = message.data.get("message")
+        self.log.info("New intercom announcement incoming!: {}".format(announcement))
         # Make a BLING sound (Might want to change this)
-        self.acknoledge()
+        self.acknowledge()
         self.speak_dialog("new.intercom", data={"message": announcement})
 
     @intent_file_handler('broadcast.intercom.intent')
@@ -39,11 +40,11 @@ class Communications(MycroftSkill):
         # Get the announcement
         announcement = message.data.get("announcement")
         while not announcement:
-            announcement = self.get_response("get.new.announcement.name.dialog")
+            announcement = self.get_response("get.new.announcement.name")
 
         # OKay, we got the announcement
         # Time to send the message to all...
-        self.send_message(announcement, "intercom")
+        self.send_intercom(announcement)
         self.speak_dialog('broadcasting.intercom')
 
 
--- shippingHandling.py
+++ shippingHandling.py
@@ -1,6 +1,6 @@
 """Part of the Communications skill to recieve messages and messagebus them over to the skill, which announces them"""
-import py2p
 import time
+
 from mycroft.messagebus.send import send
 
 
@@ -18,3 +18,16 @@ def startLoop(socket):
             message = str(msg.packets[1])
             # Send to messagebus
             send_communication_to_messagebus("intercom", message)
+
+
+def send_message(socket, message, message_type, recipients=None):
+    """TODO: REVAMP: People, intercom, specific devices
+    Args: TYPE and Message and WHO FOR (Needed if not intercom"""
+    if message_type is not "intercom" and recipients is None:
+        # Check that when there is a message, it has a specified recipient
+        raise ValueError("[communicationsSkill/shippingHandling] To send a message, you need to specify a recipient")
+    # TODO: change so that we can support more than just intercom and just one device
+    time.sleep(1)
+    if message_type is "intercom":
+        # send intercom message
+        socket.send(str(message))
